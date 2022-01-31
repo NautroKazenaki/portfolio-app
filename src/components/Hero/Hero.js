23717a0bba390316fd33dcd84634126709dd2c40
Сергей Ашарин <kalliak2014@yandex.ru>
2022-01-31 16:17:49 +0300

    beauty bg + some info

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,22 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Всем привет, <br />
+        Я - Nautro Kazenaki <br />
+        и Я - React developer!
+      </SectionTitle>
+      <SectionText>
+        Здесь можно найти всю необходимую информацию обо мне,<br />
+        мои pet-проекты, часто используемый stack <br />
+        а если хочется покликать, то сюда ↓
+      </SectionText>
+      <Button onClick={() => window.location = "https://github.com/NautroKazenaki"}>Это тут!</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
